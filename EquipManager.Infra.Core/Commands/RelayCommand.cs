fb732ad3d0deffd36b555e4b0c1b1eb023d69135
dimainnature <dimainnature@yandex.ru>
2022-05-11 15:28:58 +0400

    Removed code analyzer warnings in RelayCommand.

--- EquipManager.Infra.Core/Commands/RelayCommand.cs
+++ EquipManager.Infra.Core/Commands/RelayCommand.cs
@@ -3,9 +3,9 @@
 /// <summary> Реализация поведенческого паттерна "Command".</summary>
 public sealed class RelayCommand : ICommand
 {
-    private readonly Action<object>? _execute;
+    private readonly Action<object?>? _execute;
 
-    private readonly Func<object, bool>? _canExecute;
+    private readonly Func<object?, bool>? _canExecute;
 
     public event EventHandler? CanExecuteChanged
     {
@@ -13,17 +13,17 @@ public sealed class RelayCommand : ICommand
         remove { CommandManager.RequerySuggested -= value; }
     }
 
-    public bool CanExecute(object parameter) =>
+    public bool CanExecute(object? parameter) =>
         _canExecute is null || _canExecute(parameter);
 
-    public void Execute(object parameter) =>
+    public void Execute(object? parameter) =>
         _execute?.Invoke(parameter);
 
-    public RelayCommand(Action<object> executeAction)
+    public RelayCommand(Action<object?> executeAction)
         : this(executeAction: executeAction, canExecuteFunc: null) =>
         _execute = executeAction;
 
-    public RelayCommand(Action<object>? executeAction,
-        Func<object, bool>? canExecuteFunc) =>
+    public RelayCommand(Action<object?>? executeAction,
+        Func<object?, bool>? canExecuteFunc) =>
         (_canExecute, _execute) = (canExecuteFunc, executeAction);
 }
